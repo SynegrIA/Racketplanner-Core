cd238de2238077d486d4d66c517b2ec45856e884
ketaloca <xesuspb@gmail.com>
2025-07-01 15:35:32 +0200

    Problema naming servicio Builderbot

--- Backend/src/api/services/builderBot.js
+++ Backend/src/api/services/builderBot.js
@@ -2,10 +2,10 @@ import fetch from 'node-fetch'
 import { BUILDERBOT_URL, BUILDERBOT_KEY } from '../../config/config.js'
 
 export async function enviarMensajeWhatsApp(mensaje, numero) {
-    await fetch(BUILDERBOT_API_URL, {
+    await fetch(BUILDERBOT_URL, {
         method: 'POST',
         headers: {
-            'x-api-builderbot': BUILDERBOT_API_KEY,
+            'x-api-builderbot': BUILDERBOT_KEY,
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
