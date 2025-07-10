bced4939a8468abf0699884c5cc4fbb55c42b5b3
ketaloca <xesuspb@gmail.com>
2025-07-10 12:13:14 +0200

    Ajustando variables de entorno y deployments para adaptar dominios a "prod" o "dev"

--- Frontend/config.js
+++ Frontend/config.js
@@ -2,4 +2,4 @@ import dotenv from 'dotenv'
 
 dotenv.config
 
-export const DOMINIO_BACKEND = import.meta.env.VITE_DOMINIO_BACKEND
+export const DOMINIO_BACKEND = import.meta.env.NODE_ENV === 'production' ? import.meta.env.VITE_DOMINIO_BACKEND_PROD : import.meta.env.VITE_DOMINIO_BACKEND_DEV
