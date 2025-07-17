2e4f7466deb7d6435305f658422af20d72e26f7a
ketaloca <xesuspb@gmail.com>
2025-07-17 09:33:38 +0200

    Mejorando script redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -6,12 +6,13 @@ cd "$(dirname "$0")"
 #BACKEND
 echo 'Vamos a reconstruir el backend...'
 
-docker build --platform Linux/amd64 -t xesuspb/picketballplanner-backend:latest --push Backend
+docker buildx build --platform Linux/amd64 -t xesuspb/picketballplanner-backend:latest --push Backend
 
 echo 'Ahora vamos a redesplegar el backend'
 
-kubectl delete deployment picketball-racketplanner-backend
-kubectl apply -f k8s/backend/deployment.yaml
+#kubectl delete deployment picketball-racketplanner-backend
+#kubectl apply -f k8s/backend/deployment.yaml
+kubectl rollout restart deployment picketball-racketplanner-backend
 
 echo 'Backend reconstruido y redesplegado correctamente'
 
@@ -19,11 +20,12 @@ echo 'Backend reconstruido y redesplegado correctamente'
 #FRONTEND
 echo 'Vamos a reconstruir el frontend...'
 
-docker build --platform Linux/amd64 -t xesuspb/picketballplanner-frontend:latest --push Frontend
+docker buildx build --platform Linux/amd64 -t xesuspb/picketballplanner-frontend:latest --push Frontend
 
 echo 'Ahora vamos a redesplegar el frontend'
 
-kubectl delete deployment picketball-racketplanner-frontend
-kubectl apply -f k8s/frontend/deployment.yaml
+#kubectl delete deployment picketball-racketplanner-frontend
+#kubectl apply -f k8s/frontend/deployment.yaml
+kubectl rollout restart deployment picketball-racketplanner-frontend
 
 echo 'Frontend reconstruido y redesplegado correctamente'
