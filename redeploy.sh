58a1e53a392be748b74c658e840ed204770a3a73
ketaloca <xesuspb@gmail.com>
2025-07-17 16:11:11 +0200

    redeploy.sh ajustado

--- redeploy.sh
+++ redeploy.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 #BACKEND
 echo 'Vamos a reconstruir el backend...'
 
-docker buildx build --platform Linux/amd64 -t xesuspb/picketballplanner-backend:latest --push Backend
+docker build --platform Linux/amd64 -t xesuspb/picketballplanner-backend:latest --push Backend
 
 echo 'Ahora vamos a redesplegar el backend'
 
@@ -20,7 +20,7 @@ echo 'Backend reconstruido y redesplegado correctamente'
 #FRONTEND
 echo 'Vamos a reconstruir el frontend...'
 
-docker buildx build --platform Linux/amd64 -t xesuspb/picketballplanner-frontend:latest --push Frontend
+docker build --platform Linux/amd64 -t xesuspb/picketballplanner-frontend:latest --push Frontend
 
 echo 'Ahora vamos a redesplegar el frontend'
 
