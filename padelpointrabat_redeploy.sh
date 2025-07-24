6aac9a11dbc75720d537c7668d6d5f6850ac21d7
ketaloca <xesuspb@gmail.com>
2025-07-24 16:27:24 +0200

    .

---
+++ padelpointrabat_redeploy.sh
@@ -0,0 +1,31 @@
+set -e
+
+cd "$(dirname "$0")"
+
+
+#BACKEND
+echo 'Vamos a reconstruir el backend...'
+
+docker build --platform Linux/amd64 -t xesuspb/picketballplanner-backend:latest --push Backend
+
+echo 'Ahora vamos a redesplegar el backend'
+
+#kubectl delete deployment picketball-racketplanner-backend
+#kubectl apply -f k8s/backend/deployment.yaml
+kubectl rollout restart deployment picketball-racketplanner-backend
+
+echo 'Backend reconstruido y redesplegado correctamente'
+
+
+#FRONTEND
+echo 'Vamos a reconstruir el frontend...'
+
+docker build --platform Linux/amd64 -t xesuspb/picketballplanner-frontend:latest --push Frontend
+
+echo 'Ahora vamos a redesplegar el frontend'
+
+#kubectl delete deployment picketball-racketplanner-frontend
+#kubectl apply -f k8s/frontend/deployment.yaml
+kubectl rollout restart deployment picketball-racketplanner-frontend
+
+echo 'Frontend reconstruido y redesplegado correctamente'
